3dc96e83f93e488bd36f9f243991e2671ff5c7d4
pikkauhn <114180449+pikkauhn@users.noreply.github.com>
2023-05-11 10:48:09 -0500

    Update AboutMe.js

--- src/pages/AboutMe/AboutMe.js
+++ src/pages/AboutMe/AboutMe.js
@@ -10,7 +10,6 @@ function AboutMe() {
     const content = `Hello and thank you for visiting my portfolio! I'm Zachary Burns,<a recent Summa Cum Laude graduate of Capella University with a<Bachelor's degree in Software Development. During my time at Capella,<I consistently demonstrated a high level of skill and dedication to my<studies, earning recognition from both my professors and peers.<<I have experience working with Java, React, JavaScript,<Node.js, SQL, MySQL, and NoSQL (MongoDB), as well as various APIs.<I am currently learning Kotlin with Jetpack Compose, and I am<committed to continuing my education and professional development.<<Overall, I'm a driven and talented individual, and I'm looking forward to <making my mark in the world of software development.`;
     const detailsRef = useRef(null);
     const imgRef = useRef(null);
-    let load = false;
 
     useEffect(() => {
         let i = 0;
@@ -50,10 +49,6 @@ function AboutMe() {
             setTimeout(typing, delay);
           }
         };
-        if (load) {
-          typing();
-        }
-        load = true;
       }, [content]);
 
     useEffect(() => {
