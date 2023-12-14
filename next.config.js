b74883634a3308080fe04fa42268fd6bc93d58e0
pikkauhn <114180449+pikkauhn@users.noreply.github.com>
2023-12-14 12:06:50 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-}
-
-module.exports = {
     images: {
         unoptimized: true 
-    },
-    nextConfig
+    }
 }
+
+module.exports = nextConfig;
