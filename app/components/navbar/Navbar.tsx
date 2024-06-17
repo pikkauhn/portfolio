3a44adabc349981ee5b9ac73f9ee918ea4e87134
Zachary Burns <114180449+pikkauhn@users.noreply.github.com>
2024-06-17 16:36:42 -0500

    Update Navbar.tsx
    Added github and linkedIn profiles to navbar

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -2,13 +2,10 @@
 import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import Image from 'next/image';
-import me from '../../assets/images/me.jpg';
 import { PrimeIcons } from 'primereact/api';
 import { Sidebar } from 'primereact/sidebar';
 import { Button } from 'primereact/button';
-
-
-
+import me from '../../assets/images/me.jpg';
 import './Navbar.css';
 import useResponsiveStrings from '../useResponsiveStrings'
 
@@ -25,11 +22,15 @@ const Navbar = () => {
           <Sidebar className='sideBar' showCloseIcon={false} visible={visible} onHide={() => setVisible(false)}>
             <div className="grid">
               <span className="ml-1 col">
-                <h2 className='menuHeader'>Menu</h2>                
+                <h2 className='menuHeader'>Menu</h2>
                 <Button outlined className='menuButtons w-10 mb-2' label="About Me" onClick={() => { router.replace('/'); setVisible(false) }} />
                 <Button outlined className='menuButtons w-10 mb-2' label="Projects" onClick={() => { router.replace('/Projects'); setVisible(false) }} />
                 <Button outlined className='menuButtons w-10 mb-2' label="Education" onClick={() => { router.replace('/Education'); setVisible(false) }} />
                 <Button outlined className='menuButtons w-10' label="Experience" onClick={() => { router.replace('/Experience'); setVisible(false) }} />
+              </span>
+              <span className='ml-1'>
+                <Button outlined className='menuButtons mr-2 ml-6' onClick={() => { window.open("https://github.com/pikkauhn", '_blank') }} icon='pi pi-github' />
+                <Button outlined className='menuButtons' onClick={() => { window.open("https://www.linkedin.com/in/zachary-a-burns/", '_blank') }} icon='pi pi-linkedin' />
               </span>
             </div>
           </Sidebar>
@@ -46,6 +47,8 @@ const Navbar = () => {
               <a href="/Projects">Projects</a>
               <a href="/Education">Education</a>
               <a href="/Experience">Experience</a>
+              <a href="https://github.com/pikkauhn" target="_blank"><i className='pi pi-github' /></a>
+              <a href="https://www.linkedin.com/in/zachary-a-burns/" target="_blank"><i className='pi pi-linkedin' /></a>
             </div>
           </div>
         </div>
